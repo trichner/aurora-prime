4ed5c08f310747df41f9b3159c4fdf6954b303fc
thomasrichner <thomasrichner@gmail.com>
2012-10-31 14:38:48 +0000

    added Request

---
+++ android/AndroidPrime/src/ch/k42/androidconnect/prototype/QuorgSettings.java
@@ -0,0 +1,30 @@
+package ch.k42.androidconnect.prototype;
+
+import ch.k42.androidconnect.prototype.Request.Command;
+
+public class QuorgSettings {
+	private int screen;
+	private int quorgID;
+	private String[] settings;
+	public int getScreen() {
+		return screen;
+	}
+	public void setScreen(int screen) {
+		this.screen = screen;
+	}
+	public int getQuorgID() {
+		return quorgID;
+	}
+	public void setQuorgID(int quorgID) {
+		this.quorgID = quorgID;
+	}
+	public String[] getSettings() {
+		return settings;
+	}
+	public void setSettings(String[] settings) {
+		this.settings = settings;
+	}
+	
+	
+}
+
