3ef87566497da0f4a3276759929f6698e019a2c8
philippboesch@gmail.com <philippboesch@gmail.com@38aab2f1-4c3f-577e-8637-2e76c6a78de4>
2012-12-13 17:22:20 +0000

    main.xml: Theme is now HoloLight.VadersActionbar
    HomeActivity.java: Added butttons, Added on click listener and Intend to start ListActivity
    
    ListActivity.java: created
    List.xml: created
    
     - bert

--- trunk/android/AndroidPrime/gen/ch/k42/auroraprime/R.java
+++ trunk/android/AndroidPrime/gen/ch/k42/auroraprime/R.java
@@ -18,10 +18,10 @@ public final class R {
         public static final int button2=0x7f050001;
         public static final int button3=0x7f050002;
         public static final int button4=0x7f050003;
-        public static final int connectButton=0x7f050004;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int list=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
