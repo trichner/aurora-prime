0c22122e49f8972d3873b651b6b283866cc534ce
philippboesch <philippboesch@gmail.com>
2013-03-21 10:29:27 +0000

    -yolobert

--- android/AndroidPrime/src/ch/k42/auroraprime/main/HomeActivity.java
+++ android/AndroidPrime/src/ch/k42/auroraprime/main/HomeActivity.java
@@ -6,11 +6,45 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.content.Intent;
+import android.graphics.Rect;
 
 
-public class HomeActivity extends Activity implements OnClickListener {
+public class HomeActivity extends Activity {
+	
+	int titleHeight = 0;
+	int statusHeight = 0;
+	int contentViewTop = 0;
+	int displayHeight = 0;
+	int layoutHeight = 0;
+	int layoutWidth = 0;
+	
+	private class Button_Listener implements OnClickListener {
+
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			// get id of button pressed, send intent and start ListActivity
+			Intent i=null;
+			switch (v.getId())
+			{
+			case R.id.button1: i = new Intent(HomeActivity.this, ListActivity.class);
+							   i.putExtra("sourceButton", "button1"); break;
+			case R.id.button2: i = new Intent(HomeActivity.this, ListActivity.class);
+			   				   i.putExtra("sourceButton", "button2"); break;
+			case R.id.button3: i = new Intent(HomeActivity.this, ListActivity.class);
+							   i.putExtra("sourceButton", "button3"); break;
+			case R.id.button4: i = new Intent(HomeActivity.this, ListActivity.class);
+							   i.putExtra("sourceButton", "button4"); break;
+			}
+			Log.d(TAG, "onClicked");
+			startActivity(i);
+		}
+		
+	}
+	
 	
 	private static final String TAG = "HomeActivity";
 	Button button1;
@@ -18,44 +52,47 @@ public class HomeActivity extends Activity implements OnClickListener {
 	Button button3;
 	Button button4;
 	
-	//is the app connected to a device?
-	private boolean connected = false;
-	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        // Random modded Stack overflow code to get the space available for the active layout in view
+        LinearLayout root = (LinearLayout) findViewById(R.id.linlayout);    
+        root.post(new Runnable() {
+        	
+            public void run() {
+                Rect rect = new Rect();
+                Window win = getWindow();
+                win.getDecorView().getWindowVisibleDisplayFrame(rect);
+                statusHeight = rect.top;
+                contentViewTop = win.findViewById(Window.ID_ANDROID_CONTENT).getTop();
+                titleHeight = contentViewTop - statusHeight;
+                Log.d(TAG, "titleHeight = " + titleHeight + " statusHeight = " + statusHeight + " contentViewTop = " + contentViewTop);
+                layoutWidth = getResources().getDisplayMetrics().widthPixels;
+                displayHeight = getResources().getDisplayMetrics().heightPixels;
+                layoutHeight = displayHeight - titleHeight - statusHeight;
+                Log.d(TAG, "layoutHeight = " + layoutHeight + " layoutWidth = " + layoutWidth);
+            }
+        });
+        
+        //
+            
         //die Views Suchen
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
         button4 = (Button) findViewById(R.id.button4);
         
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
-        button4.setOnClickListener(this);
+        OnClickListener listener = new Button_Listener();
+        
+        button1.setOnClickListener(listener);
+        button2.setOnClickListener(listener);
+        button3.setOnClickListener(listener);
+        button4.setOnClickListener(listener);
+        
+       
     }
-
-public void onClick(View v) {
-	// get id of button pressed, send intent and start ListActivity
-	Intent i=null;
-	switch (v.getId())
-	{
-	case R.id.button1: i = new Intent(this, ListActivity.class);
-					   i.putExtra("sourceButton", "button1"); break;
-	case R.id.button2: i = new Intent(this, ListActivity.class);
-	   				   i.putExtra("sourceButton", "button2"); break;
-	case R.id.button3: i = new Intent(this, ListActivity.class);
-					   i.putExtra("sourceButton", "button3"); break;
-	case R.id.button4: i = new Intent(this, ListActivity.class);
-					   i.putExtra("sourceButton", "button4"); break;
-	}
-	Log.d(TAG, "onClicked");
-	startActivity(i);
-	
-}
 
 }
---
+++ android/AndroidPrime/src/ch/k42/auroraprime/net/DiscoveryThread.java
@@ -0,0 +1,66 @@
+package ch.k42.auroraprime.net;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+ 
+public class DiscoveryThread extends Thread {
+	private static final int MULTICAST_PORT=4455;
+	private static final int SOCKET_PORT=4445;
+	private static final int RCV_TIMEOUT=100;
+	private static final String MULTICAST_GROUP="225.0.0.42";
+	
+    private DatagramSocket socket;
+    
+    private List<ALDevice> list;
+    private String sender;
+    
+    public DiscoveryThread(String sender,List<ALDevice> list) throws SocketException{
+        this.sender = sender;
+        this.list = list;
+    	socket = new DatagramSocket(SOCKET_PORT);
+    }
+ 
+    public void run() {
+    	try{
+            byte[] buf = new byte[256];
+ 
+            String dString = sender;
+
+            buf = dString.getBytes();
+ 
+            // send it
+            InetAddress group;
+			group = InetAddress.getByName(MULTICAST_GROUP);
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, group, MULTICAST_PORT);
+            socket.send(packet);
+            
+            //--- wait for max 10 devices
+            byte[] rbuf;
+            socket.setSoTimeout(RCV_TIMEOUT);
+            for(int i=10; i>0; i--){
+            	try{
+            		rbuf = new byte[256];
+    			    packet = new DatagramPacket(rbuf, rbuf.length);
+    			    
+	            	socket.receive(packet);
+	            	String str = new String(packet.getData(),0,packet.getLength());
+	            	
+	            	//Log.v("Got ACK       : " + str);
+	            	//Log.v("Packet length : " + packet.getLength());
+	            	ALDevice device = new ALDevice(packet.getAddress(), str);
+	            	
+	            	list.add(device);
+            	}catch(SocketTimeoutException e){
+            		//Log.d("DISCOVERY THREAD","Socket Timeout: waiting for next package");
+            	}
+            }
+            
+    	}catch(IOException e){
+        	//Log.e(e.getMessage());
+        }
+        socket.close();
+    }
+}
+
+
