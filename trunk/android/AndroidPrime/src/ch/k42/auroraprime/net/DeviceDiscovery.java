082289b9384980cbd0ac4ddad2d6b1facfe74646
philippboesch@gmail.com <philippboesch@gmail.com@38aab2f1-4c3f-577e-8637-2e76c6a78de4>
2013-03-21 10:29:27 +0000

    -yolobert

---
+++ trunk/android/AndroidPrime/src/ch/k42/auroraprime/net/DeviceDiscovery.java
@@ -0,0 +1,36 @@
+package ch.k42.auroraprime.net;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DeviceDiscovery implements IDeviceDiscovery {
+	private final static int TIMEOUT = 5000;
+	//@Override
+	public List<ALDevice> getDiscoveredDevices() {
+		DiscoveryThread test;
+		List<ALDevice> list = new ArrayList<ALDevice>();
+		try {
+			test = new DiscoveryThread("myPhone", list);
+			test.start();
+			
+			test.join(TIMEOUT);
+			
+			
+			//---- print discovered devices
+			//Log.v("Found devices:");
+			for(ALDevice dev : list){
+				//Log.d("DEVICE DISCOVERY",dev);
+			}
+		} catch (SocketException e) {
+			//Log.e(e.getMessage());
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			//Log.e(e.getMessage());
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+
+}
