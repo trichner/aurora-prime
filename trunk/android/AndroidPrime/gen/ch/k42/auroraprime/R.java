431ab929bdca680aa955ef003e3666928634f448
philippboesch@gmail.com <philippboesch@gmail.com@38aab2f1-4c3f-577e-8637-2e76c6a78de4>
2013-04-08 16:33:50 +0000

    AndroidPrime.apk classes.dex resources.ap_ main.xml strings.xml HomeActivity.java  -bert

--- trunk/android/AndroidPrime/gen/ch/k42/auroraprime/R.java
+++ trunk/android/AndroidPrime/gen/ch/k42/auroraprime/R.java
@@ -18,11 +18,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f060002;
-        public static final int button2=0x7f060003;
-        public static final int button3=0x7f060004;
-        public static final int button4=0x7f060005;
-        public static final int buttonRow1=0x7f060001;
+        public static final int B1_1=0x7f060005;
+        public static final int B1_2=0x7f060006;
+        public static final int B1_3=0x7f060007;
+        public static final int B1_4=0x7f060008;
+        public static final int B2_1=0x7f060009;
+        public static final int B2_2=0x7f06000a;
+        public static final int B2_3=0x7f06000b;
+        public static final int B2_4=0x7f06000c;
+        public static final int button1=0x7f060001;
+        public static final int button2=0x7f060002;
+        public static final int button3=0x7f060003;
+        public static final int button4=0x7f060004;
         public static final int linlayout=0x7f060000;
     }
     public static final class layout {
@@ -30,6 +37,22 @@ public final class R {
         public static final int main=0x7f030001;
     }
     public static final class string {
+        public static final int B1_1=0x7f050007;
+        public static final int B1_2=0x7f050008;
+        public static final int B1_3=0x7f050009;
+        public static final int B1_4=0x7f05000a;
+        public static final int B2_1=0x7f05000b;
+        public static final int B2_2=0x7f05000c;
+        public static final int B2_3=0x7f05000d;
+        public static final int B2_4=0x7f05000e;
+        public static final int B3_1=0x7f05000f;
+        public static final int B3_2=0x7f050010;
+        public static final int B3_3=0x7f050011;
+        public static final int B3_4=0x7f050012;
+        public static final int B4_1=0x7f050013;
+        public static final int B4_2=0x7f050014;
+        public static final int B4_3=0x7f050015;
+        public static final int B4_4=0x7f050016;
         public static final int app_name=0x7f050001;
         public static final int button1=0x7f050002;
         public static final int button2=0x7f050003;
