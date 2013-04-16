8f75122cca91496a7f0a0bf0f8cde17512d345ae
thomasrichner <thomasrichner@gmail.com>
2013-04-16 11:41:22 +0000

    added all implementations

---
+++ android/AndroidPrime/src/ch/k42/auroraprime/net/AthmosClient.java
@@ -0,0 +1,48 @@
+package ch.k42.auroraprime.net;
+
+import java.io.*;
+import java.net.*;
+
+
+public class AthmosClient implements IClient {
+	private static final String TAG = "Client";
+	private Socket socket;
+	private boolean connected = false;
+	private ObjectInputStream in;
+	private ObjectOutputStream out;
+	
+	public void connect(String ip, int port) throws IOException {
+			socket = new Socket();
+			SocketAddress sockaddr = new InetSocketAddress(ip, port);
+			socket.connect(sockaddr, 3000);
+			out = new ObjectOutputStream(socket.getOutputStream());
+			in = new ObjectInputStream(socket.getInputStream());
+			connected = true;
+	}
+
+	public void disconnect() {
+		try {
+			connected = false;
+			socket.close();
+		} catch (Exception e) {
+			
+		}
+	}
+
+	public boolean isConnected() {
+		return connected;
+	}
+
+	public Object sendRequest(Object request) {
+		try {
+			out.reset();
+			out.writeObject(request);
+			out.flush(); 				// Important! else TCP waits for further data to send a package
+			Object o = in.readObject(); // await a response from the server
+			return o;
+		} catch (Exception e) {
+		}
+		return null;
+	}
+}
+
