ec75e59629971b722f8e0c068ca580dc3b0cd0f2
thomasrichner@gmail.com <thomasrichner@gmail.com@38aab2f1-4c3f-577e-8637-2e76c6a78de4>
2013-04-16 18:23:54 +0000

    added factories, deleted deprecated classes

--- trunk/android/AndroidPrime/src/ch/k42/auroraprime/net/DeviceDiscoveryFactory.java
+++ trunk/android/AndroidPrime/src/ch/k42/auroraprime/net/DeviceDiscoveryFactory.java
@@ -1,6 +1,6 @@
 package ch.k42.auroraprime.net;
 
-public class IDeviceDiscoveryFactory {
+public class DeviceDiscoveryFactory {
 	public static IDeviceDiscovery getInstance(){
 		return new DeviceDiscovery();
 	}
