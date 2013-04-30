38ac79beb9c93331605d1d31ffef1faf303962a9
thomasrichner@gmail.com <thomasrichner@gmail.com@38aab2f1-4c3f-577e-8637-2e76c6a78de4>
2013-04-30 13:04:25 +0000

    added dtos

---
+++ trunk/android/AndroidPrime/src/ch/k42/auroraprime/dto/DeviceState.java
@@ -0,0 +1,29 @@
+package ch.k42.auroraprime.net.DTO;
+
+import java.util.List;
+import java.util.SortedMap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomas
+ * Date: 4/30/13
+ * Time: 12:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DeviceState {
+    private SortedMap<Integer,QuorgSettings> quorgs;
+    private List<Integer> matrices;
+
+    public DeviceState(SortedMap<Integer,QuorgSettings> quorgs, List<Integer> matrices){
+        this.quorgs = quorgs;
+        this.matrices = matrices;
+    }
+
+    public SortedMap<Integer, QuorgSettings> getQuorgs() {
+        return quorgs;
+    }
+
+    public List<Integer> getMatrices() {
+        return matrices;
+    }
+}
