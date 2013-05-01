db18d4eebe8b9e2f5aed554f19167babbc5d6f44
philippboesch@gmail.com <philippboesch@gmail.com@38aab2f1-4c3f-577e-8637-2e76c6a78de4>
2013-05-01 15:06:47 +0000

    Refresh Quorg Button Icons on changed Quorg Refresh Field State on changed Quorg More Quorgs and images Added
    also, Steve is a fag

--- trunk/android/AndroidPrime/src/ch/k24/auroraprime/quorg/StaticQuorgListGenerator.java
+++ trunk/android/AndroidPrime/src/ch/k24/auroraprime/quorg/StaticQuorgListGenerator.java
@@ -21,25 +21,62 @@ public class StaticQuorgListGenerator implements IQuorgListGenerator {
 		// TODO Auto-generated method stub
 		list = new ArrayList<Quorg>();
 		
+		//no Quoq defionition
+		
+		Quorg emptyQuorg = new Quorg(R.drawable.ic_field_000,QUORG.OFF, "Empty Quorg", false);
+		list.add(emptyQuorg);
+		
 		//Red LED Quorg definition
 		
-		Quorg redLedQuorg = new Quorg(R.drawable.redlight,QUORG.REDLIGHT, "Red LED Field", false);
+		Quorg redLedQuorg = new Quorg(R.drawable.ic_field_001,QUORG.REDLIGHT, "Red LED Field", false);
 		list.add(redLedQuorg);
 		
 		//Green LED Quorg definition
 		
-		Quorg greenLedQuorg = new Quorg(R.drawable.greenlight,QUORG.GREENLIGHT, "Green LED Field", false);
+		Quorg greenLedQuorg = new Quorg(R.drawable.ic_field_002,QUORG.GREENLIGHT, "Green LED Field", false);
 		list.add(greenLedQuorg);
 				
 		//Blue LED Quorg definition
 				
-		Quorg blueLedQuorg = new Quorg(R.drawable.bluelight,QUORG.BLUELIGHT, "Blue LED Field", false);
+		Quorg blueLedQuorg = new Quorg(R.drawable.ic_field_003,QUORG.BLUELIGHT, "Blue LED Field", false);
 		list.add(blueLedQuorg);
 		
 		//Clock Quorg definition
 		
-		Quorg clockQuorg = new Quorg(R.drawable.clock,QUORG.CLOCK, "Clock", true);
+		Quorg clockQuorg = new Quorg(R.drawable.ic_field_004,QUORG.CLOCK, "Clock", false);
 		list.add(clockQuorg);
+		
+		
+		//random Quoq defionition
+		
+		Quorg randomQuorg = new Quorg(R.drawable.ic_field_005,QUORG.RANDOM, "Random Dots", false);
+		list.add(randomQuorg);
+		
+		//mail Quorg definition
+		
+		Quorg mailQuorg = new Quorg(R.drawable.ic_field_006,QUORG.MAIL, "E-Mail Notifier", true);
+		list.add(mailQuorg);
+		
+		//weather LED Quorg definition
+		
+		Quorg weatherQuorg = new Quorg(R.drawable.ic_field_007,QUORG.WEATHER, "Weather", true);
+		list.add(weatherQuorg);
+				
+		//matrix LED Quorg definition
+				
+		Quorg matrixQuorg = new Quorg(R.drawable.ic_field_008,QUORG.MATRIX, "Matrix Effevt", false);
+		list.add(matrixQuorg);
+		
+		//audiovisualizer Quorg definition
+		
+		Quorg visualizerQuorg = new Quorg(R.drawable.ic_field_009,QUORG.AUDIOVISUALIZER, "Audiovisualizer", false);
+		list.add(visualizerQuorg);
+		
+		//Reddit Quorg definition
+		
+		Quorg redditQuorg = new Quorg(R.drawable.ic_field_010,QUORG.REDDIT, "Karma Train Notifier", true);
+		list.add(redditQuorg);
+		
 
 		return list;
 	}
