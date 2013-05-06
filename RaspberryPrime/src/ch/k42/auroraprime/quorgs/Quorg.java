c9fd20c34f31dda2ee54d95d1476acc2bfd4c11b
thomasrichner <thomasrichner@gmail.com>
2013-05-06 02:37:32 +0000

    implemented MatrixManager fully, makes Main smaller

--- RaspberryPrime/src/ch/k42/auroraprime/quorgs/Quorg.java
+++ RaspberryPrime/src/ch/k42/auroraprime/quorgs/Quorg.java
@@ -1,9 +1,14 @@
 package ch.k42.auroraprime.quorgs;
 
+import ch.k42.auroraprime.net.DTO.QuorgSettings;
+
 public abstract class Quorg extends Thread {
 	public abstract IFrame8x8 getFrame();
 	protected volatile boolean quit = true;
+    protected QuorgSettings settings;
+
     private int MatrixID = -1;
+
 
     public void setMatrixID(int matrixID){
         this.MatrixID = matrixID;
@@ -23,4 +28,7 @@ public abstract class Quorg extends Thread {
 	
 	@Override
 	public abstract void run();
+
+    public abstract void initSettings(String[] settings);
+    public abstract String[] getSettings();
 }
